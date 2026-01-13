4df442b4500fd84fb37e8422e1a9bd229a4ae806
jinyoung02 <jink8962@naver.com>
2026-01-13 18:31:10 +0900

    refactor: RecruitContent 조회 메서드 제거

--- src/main/java/com/likelionknu/applyserver/recruit/data/repository/RecruitContentRepository.java
+++ src/main/java/com/likelionknu/applyserver/recruit/data/repository/RecruitContentRepository.java
@@ -3,9 +3,5 @@ package com.likelionknu.applyserver.recruit.data.repository;
 import com.likelionknu.applyserver.recruit.data.entity.RecruitContent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RecruitContentRepository extends JpaRepository<RecruitContent, Long> {
-
-    List<RecruitContent> findByRecruitIdOrderByPriorityAsc(Long recruitId);
 }
