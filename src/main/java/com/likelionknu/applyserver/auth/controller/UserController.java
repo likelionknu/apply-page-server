cde937a7f7455770370421b9b0f21394c727fd37
Yun-Hwan Jeon <yunsol267@gmail.com>
2026-01-13 14:03:39 +0900

    feat: UserController Base URL 변경 (#5)

--- src/main/java/com/likelionknu/applyserver/auth/controller/UserController.java
+++ src/main/java/com/likelionknu/applyserver/auth/controller/UserController.java
@@ -4,22 +4,25 @@ import com.likelionknu.applyserver.auth.data.dto.request.ModifyProfileRequestDto
 import com.likelionknu.applyserver.auth.data.dto.response.ProfileResponseDto;
 import com.likelionknu.applyserver.auth.service.UserService;
 import com.likelionknu.applyserver.common.response.GlobalResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @DeleteMapping("/me")
+    @Operation(summary = "사용자 회원탈퇴")
     public GlobalResponse<Void> deleteUsersProfile(String email) {
         userService.deleteUsersProfile(email);
         return GlobalResponse.ok();
     }
 
     @PatchMapping("/me/profile")
+    @Operation(summary = "사용자 프로필 등록(변경)")
     public GlobalResponse<Void> modifyUsersProfile(String email,
                                                    @RequestBody ModifyProfileRequestDto modifyProfileRequestDto) {
         userService.modifyUsersProfile(email, modifyProfileRequestDto);
@@ -27,6 +30,7 @@ public class UserController {
     }
 
     @GetMapping("/me/profile")
+    @Operation(summary = "사용자 프로필 조회")
     public GlobalResponse<ProfileResponseDto> getUsersProfile(String email) {
         return GlobalResponse.ok(userService.getUsersProfile(email));
     }
