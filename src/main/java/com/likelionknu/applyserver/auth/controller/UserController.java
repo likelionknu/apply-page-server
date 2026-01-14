841bdfb75a21506d11d4230d069881730aba2338
Yun-Hwan Jeon <yunsol267@gmail.com>
2026-01-14 23:49:30 +0900

    feat: 사용자 조회 시 JWT 인증 사용 (#5)

--- src/main/java/com/likelionknu/applyserver/auth/controller/UserController.java
+++ src/main/java/com/likelionknu/applyserver/auth/controller/UserController.java
@@ -4,6 +4,7 @@ import com.likelionknu.applyserver.auth.data.dto.request.ModifyProfileRequestDto
 import com.likelionknu.applyserver.auth.data.dto.response.ProfileResponseDto;
 import com.likelionknu.applyserver.auth.service.UserService;
 import com.likelionknu.applyserver.common.response.GlobalResponse;
+import com.likelionknu.applyserver.common.security.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,21 +18,21 @@ public class UserController {
 
     @DeleteMapping("/me")
     @Operation(summary = "사용자 회원탈퇴")
-    public GlobalResponse<Void> deleteUsersProfile(String email) {
-        userService.deleteUsersProfile(email);
+    public GlobalResponse<Void> deleteUsersProfile() {
+        userService.deleteUsersProfile(SecurityUtil.getUsername());
         return GlobalResponse.ok();
     }
 
     @PatchMapping("/me/profile")
     @Operation(summary = "사용자 프로필 등록(변경)")
-    public GlobalResponse<ProfileResponseDto> modifyUsersProfile(@RequestParam("email") String email,
-                                                   @Valid @RequestBody ModifyProfileRequestDto modifyProfileRequestDto) {
-        return GlobalResponse.ok(userService.modifyUsersProfile(email, modifyProfileRequestDto));
+    public GlobalResponse<ProfileResponseDto> modifyUsersProfile(
+            @Valid @RequestBody ModifyProfileRequestDto modifyProfileRequestDto) {
+        return GlobalResponse.ok(userService.modifyUsersProfile(SecurityUtil.getUsername(), modifyProfileRequestDto));
     }
 
     @GetMapping("/me/profile")
     @Operation(summary = "사용자 프로필 조회")
-    public GlobalResponse<ProfileResponseDto> getUsersProfile(String email) {
-        return GlobalResponse.ok(userService.getUsersProfile(email));
+    public GlobalResponse<ProfileResponseDto> getUsersProfile() {
+        return GlobalResponse.ok(userService.getUsersProfile(SecurityUtil.getUsername()));
     }
 }
