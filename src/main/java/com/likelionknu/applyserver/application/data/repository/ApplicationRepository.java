70461faf9838dced9905c56a0a0016fd204c4cfd
SeungyeonWoo <seungyeon3287@gmail.com>
2026-01-20 17:40:54 +0900

    feat: 지원서 상태 기반 조회 기능 추가 (#7)

--- src/main/java/com/likelionknu/applyserver/application/data/repository/ApplicationRepository.java
+++ src/main/java/com/likelionknu/applyserver/application/data/repository/ApplicationRepository.java
@@ -1,6 +1,7 @@
 package com.likelionknu.applyserver.application.data.repository;
 
 import com.likelionknu.applyserver.application.data.entity.Application;
+import com.likelionknu.applyserver.auth.data.entity.User;
 import com.likelionknu.applyserver.auth.data.enums.ApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
-    Optional<Application> findFirstByStatus(ApplicationStatus status);
+    Optional<Application> findFirstByUserAndStatus(User user, ApplicationStatus status);
+
 }
