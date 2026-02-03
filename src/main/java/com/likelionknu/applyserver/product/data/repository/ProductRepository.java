76a0563d6add1256451623572eda5525a88d4777
Yun-Hwan Jeon <yunsol267@gmail.com>
2026-02-03 15:12:02 +0900

    feat: 임시 API 제거 (#42)

--- src/main/java/com/likelionknu/applyserver/product/data/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.likelionknu.applyserver.product.data.repository;
-
-import com.likelionknu.applyserver.product.data.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
