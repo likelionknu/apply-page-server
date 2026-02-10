8b1794def4f8ddb679514304efe63cc43ba6a5f2
Minwoo You <93767014+faid011@users.noreply.github.com>
2026-02-10 19:10:54 +0900

    feat: add admin recruit detail and summary 조회 API

--- src/main/java/com/likelionknu/applyserver/admin/controller/AdminRecruitController.java
+++ src/main/java/com/likelionknu/applyserver/admin/controller/AdminRecruitController.java
@@ -1,11 +1,14 @@
 package com.likelionknu.applyserver.admin.controller;
 
 import com.likelionknu.applyserver.admin.data.dto.response.AdminRecruitDetailResponse;
+import com.likelionknu.applyserver.admin.data.dto.response.AdminRecruitSummaryResponse;
 import com.likelionknu.applyserver.admin.service.AdminRecruitService;
 import com.likelionknu.applyserver.common.response.GlobalResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +21,11 @@ public class AdminRecruitController {
     @Operation(summary = "모집 공고 상세 정보 조회")
     public GlobalResponse<AdminRecruitDetailResponse> getRecruitDetail(@PathVariable Long id) {
         return GlobalResponse.ok(adminRecruitService.getRecruitDetail(id));
+    }
+
+    @GetMapping
+    @Operation(summary = "모집 공고 현황 조회")
+    public GlobalResponse<List<AdminRecruitSummaryResponse>> getRecruitSummaries() {
+        return GlobalResponse.ok(adminRecruitService.getRecruitSummaries());
     }
 }
