a403feae05ea25feba9b094561477d93577dd306
tangerinem <redvelvet0625@gmail.com>
2026-03-29 00:44:18 +0900

    refactor: migration DiscordNotificationService to kotlin

--- src/main/java/com/likelionknu/applyserver/discord/data/enums/ChannelDivider.java
+++
@@ -1,6 +0,0 @@
-package com.likelionknu.applyserver.discord.data.enums;
-
-public enum ChannelDivider {
-    USER,
-    APPLICATION
-}
---
+++ src/main/kotlin/com/likelionknu/applyserver/discord/data/enums/ChannelDivider.kt
@@ -0,0 +1,6 @@
+package com.likelionknu.applyserver.discord.data.enums
+
+enum class ChannelDivider {
+    USER,
+    APPLICATION
+}
